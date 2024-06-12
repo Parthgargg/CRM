24ed9f5192b5611d3163877670bf558658c0cbee
Parth garg <100117683+Parthgargg@users.noreply.github.com>
2024-06-12 22:05:36 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -5,12 +5,7 @@ import bookRoute from "./routes/bookRoute.js";
 import cors from "cors";
 const app = express();
 app.use(express.json()); // set the server to accept json data
-app.use(cors({
-    origin:["https://crm-api-ruby.vercel.app/"],
-    methods:["POST","GET"],
-    credetials:true
-}
-            ));
+app.use(cors());
 
 const PORT = process.env.PORT;
 
